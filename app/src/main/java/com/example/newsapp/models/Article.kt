76f1c86cad03b42712cbafaefab6ced3a8fadcb6
jamerson Macedo <jamerson_macedo@hotmail.com>
2024-01-25 21:16:46 -0300

    database configurado

--- app/src/main/java/com/example/newsapp/models/Article.kt
+++ app/src/main/java/com/example/newsapp/models/Article.kt
@@ -1,6 +1,15 @@
 package com.example.newsapp.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
@@ -9,4 +18,6 @@ data class Article(
     val title: String,
     val url: String,
     val urlToImage: String
-)
+
+    // QUANDO TEMOS UM OBJETO PARA SALVAR COMO O SOURCE É PRECISO USAR O TYPE CONVERT
+) : Serializable
